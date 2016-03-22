db92d53fab1cba7336c727463f247821b9fe19a8
Anton Galitsyn <anton.galitsyn@gmail.com>
2016-03-22 14:55:38 +0600

    rc-2

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -98,6 +98,7 @@ gulp.task('serve', ['styles', 'scripts', 'fonts'], () => {
 
   gulp.watch([
     'app/*.html',
+    'app/slides.md',
     'app/images/**/*',
     '.tmp/fonts/**/*'
   ]).on('change', reload);
