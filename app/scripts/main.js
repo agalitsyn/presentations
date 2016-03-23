74f6f42008188363e2c07efbce5e5567b8086e89
Anton Galitsyn <anton.galitsyn@gmail.com>
2016-03-23 11:31:40 +0600

    rc-3

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,8 @@
 /*eslint no-unused-vars: 0*/
 /*eslint no-undef: 0*/
 var slideshow = remark.create({
+    highlightStyle: 'github',
+    highlightLanguage: 'remark',
+    highlightLines: true,
     ratio: '16:9'
 });
